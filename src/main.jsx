d9a9b8f45fc641453ec5df7f23544ff623a93856
Anton Lysenko <antonlysenko161@yandex.ru>
2025-10-13 20:22:05 +0300

    Init commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+import store from './store/store.js'
+import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
